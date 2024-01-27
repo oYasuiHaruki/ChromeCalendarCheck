0bfb74a99bc177e0fade62ab6fe8bd83c3c5ba87
Haruki Yasui <yasui.haruki.74s@gmail.com>
2024-01-27 14:08:41 +0900

    背景を赤に変える

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,10 @@
-// Collect Eventsボタンがクリックされたときに実行されるイベントリスナーを追加する
-document.getElementById('collectEvents').addEventListener('click', () => {
-    // 現在、アクティブなタブを取得する
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        // アクティブなタブにメッセージを送信し、イベントデータの取得を要求します。
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'FETCH_EVENTS'}, (response) => {
-            // 応答を受け取った場合、その内容を処理する
-            if(response && response.status === 'Success') {
-                // "eventsList"というIDを持つ要素を取得します。
-                const eventsList = document.getElementById('eventsList');
-                eventsList.textContent = JSON.stringify(response.data, null, 2);
-            }
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError.message);
-                return;
-            }
-        });
-    });
-});
+document.addEventListener('DOMContentLoaded', function() {
+  var checkButton = document.getElementById('collectEvents');
+  checkButton.addEventListener('click', function() {
+    changeBackgroundColorToRed();
+  }, false);
+}, false);
+
+function changeBackgroundColorToRed() {
+  document.body.style.backgroundColor = 'red';
+}
